25d2ee53d1ec3b47cadf93925d95853a1c096e83
kuked <kedoku@gmail.com>
2022-02-11 14:47:46 +0900

    Add a new class Sqr::VM

--- lib/sqr.rb
+++ lib/sqr.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "sqr/version"
+require_relative "sqr/vm"
 
 module Sqr
   class Error < StandardError; end
---
+++ lib/sqr/vm.rb
@@ -0,0 +1,7 @@
+module Sqr
+  class VM
+    def hello
+      "I, VM"
+    end
+  end
+end
