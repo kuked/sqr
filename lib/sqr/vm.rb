5cb6272b24da721292a2028ab6bc7812f760409a
kuked <kedoku@gmail.com>
2022-02-12 18:39:52 +0900

    Move functions to VM

--- lib/sqr/vm.rb
+++ lib/sqr/vm.rb
@@ -1,7 +1,19 @@
 module Sqr
   class VM
-    def hello
-      "I, VM"
+    def do(input)
+      if input.start_with? "."
+        meta_command(input)
+      end
+    end
+
+    private
+
+    def meta_command(input)
+      if input == '.exit'
+        exit(0)
+      else
+        puts "Unrecognized command '#{input}'"
+      end
     end
   end
 end
